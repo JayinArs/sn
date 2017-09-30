fc7f4cf286f86a7624cf20bc173104a5fa98cf39
AbydArs <abidr.w@gmail.com>
2017-09-30 07:27:48 +0500

    update policy

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,8 +1,28 @@
 @extends('layouts.auth')
 
+@section('style')
+    <style>
+        body {
+            background-color: #5d9cec;
+        }
+    </style>
+@endsection
+
 @section('content')
-    <p>14 Pearls is committed to providing quality services to you and this policy outlines our ongoing obligations to you in respect of how we manage your Personal Information.</p>
-    <h2>What we require?</h2>
-    <p>We require your device's location to fetch near by events.</p>
-    <p>We require your device's information to create an identity in our system.</p>
+    <div class="block-center wd-xl">
+        <!-- START panel-->
+        <div class="panel panel-dark panel-flat">
+            <div class="panel-heading text-center">
+                <h3 class="m0">Privacy Policy</h3>
+            </div>
+            <div class="panel-body">
+                <p>14 Pearls is committed to providing quality services to you and this policy outlines our ongoing
+                    obligations to you in respect of how we manage your Personal Information.</p>
+                <h2>What we require?</h2>
+                <p>We require your device's location to fetch near by events.</p>
+                <p>We require your device's information to create an identity in our system.</p>
+            </div>
+        </div>
+        <!-- END panel-->
+    </div>
 @endsection
