29cb9e9e62f3f3ac095ffa500d343d4a30f44a9c
AbydArs <abidr.w@gmail.com>
2017-10-06 09:05:21 +0500

    update language logic

--- app/Http/Controllers/ApiPostController.php
+++ app/Http/Controllers/ApiPostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Language;
+use App\Localization;
 use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
@@ -12,9 +14,24 @@ class ApiPostController extends Controller
 	/**
 	 * @return mixed
 	 */
-	public function random()
+	public function random( Request $request )
 	{
-		$post = Post::with('meta_data')->inRandomOrder()->first();
-		return JSONResponse::encode(Config::get('constants.HTTP_CODES.SUCCESS'), $post);
+		$post = Post::with( 'meta_data' )->inRandomOrder()->first();
+
+		if ( $request->hasHeader( 'Content-Type' ) ) {
+			$language = Language::where( 'code', $request->header( 'Content-Type' ) )->first();
+
+			if ( $language ) {
+				$content = Localization::where( 'item_type', 'post' )
+				                       ->where( 'item_id', $post->id )
+				                       ->where( 'language_id', $language->id )
+				                       ->first( [ 'item_value' ] );
+				if ( $content ) {
+					$post->content = $content->item_value;
+				}
+			}
+		}
+
+		return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.SUCCESS' ), $post );
 	}
 }
