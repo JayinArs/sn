ac4cdc6736af726b57ce0c082d86dda7e5ef5326
AbydArs <abidr.w@gmail.com>
2017-10-07 16:36:11 +0500

    update org apis

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -24,37 +24,40 @@ class AccountController extends Controller
 	 *
 	 * @return mixed
 	 */
-	public function login(Request $request)
+	public function login( Request $request )
 	{
 		$validation_rules = [
-			'email' => 'required|exists:accounts,email',
+			'email'    => 'required|exists:accounts,email',
 			'password' => 'required',
-			'user_id' => 'required|exists:users,id'
+			'user_id'  => 'required|exists:users,id'
 		];
 
-		$validator = Validator::make($request->all(), $validation_rules);
-		$messages = $validator->messages()->all();
+		$validator = Validator::make( $request->all(), $validation_rules );
+		$messages  = $validator->messages()->all();
 
-		if ($validator->fails())
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.FAILED'), null, MultiLang::getPhrase($messages[0]));
+		if ( $validator->fails() ) {
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.FAILED' ), null, MultiLang::getPhrase( $messages[0] ) );
+		}
 
-		$user_id = $request->input('user_id');
-		$user = User::find($user_id);
+		$user_id = $request->input( 'user_id' );
+		$user    = User::find( $user_id );
 
-		if (empty($user))
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.NOT_FOUND'), null, MultiLang::getPhraseByKey('strings.user.not_found'));
+		if ( empty( $user ) ) {
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.NOT_FOUND' ), null, MultiLang::getPhraseByKey( 'strings.user.not_found' ) );
+		}
 
-		if (auth()->attempt([
-			'email' => $request->input('email'),
-			'password' => $request->input('password')
-		])) {
+		if ( auth()->attempt( [
+			                      'email'    => $request->input( 'email' ),
+			                      'password' => $request->input( 'password' )
+		                      ] )
+		) {
 
 			$account = Auth::user();
 
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.SUCCESS'), $account);
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.SUCCESS' ), $account );
 		}
 
-		return JSONResponse::encode(Config::get('constants.HTTP_CODES.NOT_FOUND'), null, MultiLang::getPhraseByKey('strings.account.not_found'));
+		return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.NOT_FOUND' ), null, MultiLang::getPhraseByKey( 'strings.account.not_found' ) );
 	}
 
 	/**
@@ -62,111 +65,119 @@ class AccountController extends Controller
 	 *
 	 * @return mixed
 	 */
-	public function register(Request $request)
+	public function register( Request $request )
 	{
 		$validation_rules = [
-			'email' => 'required|unique:accounts,email',
+			'email'    => 'required|unique:accounts,email',
 			'password' => 'required',
-			'user_id' => 'required|exists:users,id'
+			'user_id'  => 'required|exists:users,id'
 		];
 
-		$validator = Validator::make($request->all(), $validation_rules);
-		$messages = $validator->messages()->all();
+		$validator = Validator::make( $request->all(), $validation_rules );
+		$messages  = $validator->messages()->all();
 
-		if ($validator->fails())
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.FAILED'), null, MultiLang::getPhrase($messages[0]));
+		if ( $validator->fails() ) {
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.FAILED' ), null, MultiLang::getPhrase( $messages[0] ) );
+		}
 
-		$user_id = $request->input('user_id');
-		$user = User::find($user_id);
+		$user_id = $request->input( 'user_id' );
+		$user    = User::find( $user_id );
 
-		if (empty($user))
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.NOT_FOUND'), null, MultiLang::getPhraseByKey('strings.user.not_found'));
+		if ( empty( $user ) ) {
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.NOT_FOUND' ), null, MultiLang::getPhraseByKey( 'strings.user.not_found' ) );
+		}
 
-		$account = Account::create([
-			'username' => $request->input('username'),
-			'email' => $request->input('email'),
-			'password' => bcrypt($request->input('password')),
-			'role_id' => UserRole::getDefaultRole()->id,
-			'registration_date' => Carbon::now()->toDateTimeString()
-		]);
+		$account = Account::create( [
+			                            'username'          => $request->input( 'username' ),
+			                            'email'             => $request->input( 'email' ),
+			                            'password'          => bcrypt( $request->input( 'password' ) ),
+			                            'role_id'           => UserRole::getDefaultRole()->id,
+			                            'registration_date' => Carbon::now()->toDateTimeString()
+		                            ] );
 
 		$user->account_id = $account->id;
 
-		if($account->id > 0 && $user->save()) {
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.SUCCESS'), $account);
+		if ( $account->id > 0 && $user->save() ) {
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.SUCCESS' ), $account );
 		} else {
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.FAILED'), null, MultiLang::getPhraseByKey('strings.account.creation_failed'));
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.FAILED' ), null, MultiLang::getPhraseByKey( 'strings.account.creation_failed' ) );
 		}
 	}
 
 	/**
 	 * @param $id
+	 *
 	 * @return mixed
 	 */
-	private function get_account($id)
+	private function get_account( $id )
 	{
-		return Account::withTrashed()->find($id);
+		return Account::withTrashed()->find( $id );
 	}
 
 	/**
 	 * @param $id
+	 *
 	 * @return mixed
 	 */
-	public function reActivate($id)
+	public function reActivate( $id )
 	{
-		$account = $this->get_account($id);
+		$account = $this->get_account( $id );
 
 		$account->status = 1;
 
-		if($account->save() && $account->restore()) {
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.SUCCESS'), null, MultiLang::getPhraseByKey('strings.account.reactivated'));
+		if ( $account->save() && $account->restore() ) {
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.SUCCESS' ), null, MultiLang::getPhraseByKey( 'strings.account.reactivated' ) );
 		} else {
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.FAILED'), null, MultiLang::getPhraseByKey('strings.account.reactivation_failed'));
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.FAILED' ), null, MultiLang::getPhraseByKey( 'strings.account.reactivation_failed' ) );
 		}
 	}
 
 	/**
 	 * @param $id
 	 * @param Request $request
+	 *
 	 * @return mixed
 	 */
-	public function updateAccountById($id, Request $request)
+	public function updateAccountById( $id, Request $request )
 	{
-		$account = $this->get_account($id);
-		$data = $request->all();
+		$account = $this->get_account( $id );
+		$data    = $request->all();
 
-		if(!empty($data['password']))
-			$data['password'] = bcrypt($data['password']);
+		if ( ! empty( $data['password'] ) ) {
+			$data['password'] = bcrypt( $data['password'] );
+		}
 
-		$account->fill($data);
+		$account->fill( $data );
 
-		if($account->save()) {
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.SUCCESS'), null, MultiLang::getPhraseByKey('strings.account.update_success'));
+		if ( $account->save() ) {
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.SUCCESS' ), null, MultiLang::getPhraseByKey( 'strings.account.update_success' ) );
 		} else {
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.FAILED'), null, MultiLang::getPhraseByKey('strings.account.update_failed'));
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.FAILED' ), null, MultiLang::getPhraseByKey( 'strings.account.update_failed' ) );
 		}
 	}
 
 	/**
 	 * @param $id
+	 *
 	 * @return mixed
 	 */
-	public function deleteAccountById($id)
+	public function deleteAccountById( $id )
 	{
-		$account = $this->get_account($id);
+		$account = $this->get_account( $id );
 
-		if(!$account)
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.NOT_FOUND'), null, MultiLang::getPhraseByKey('strings.account.not_found'));
+		if ( ! $account ) {
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.NOT_FOUND' ), null, MultiLang::getPhraseByKey( 'strings.account.not_found' ) );
+		}
 
 		$account->status = 0;
 
 		$account->save();
 		$account->delete();
 
-		if($account->trashed()) {
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.SUCCESS'), null, MultiLang::getPhraseByKey('strings.account.destroyed'));
+		if ( $account->trashed() ) {
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.SUCCESS' ), null, MultiLang::getPhraseByKey( 'strings.account.destroyed' ) );
 		} else {
-			return JSONResponse::encode(Config::get('constants.HTTP_CODES.FAILED'), null, MultiLang::getPhraseByKey('strings.account.destroy_failed'));
+			return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.FAILED' ), null, MultiLang::getPhraseByKey( 'strings.account.destroy_failed' ) );
 		}
 	}
 
@@ -175,24 +186,29 @@ class AccountController extends Controller
 	 *
 	 * @return mixed
 	 */
-	public function accountOrganizations($id)
+	public function accountOrganizations( $id )
 	{
 		$organizations = [];
 
-		Organization::with('meta_data')->where('account_id', $id)->each(function($organization) use (&$organizations) {
+		Organization::with( 'meta_data' )->where( 'account_id', $id )->each( function ( $organization ) use ( &$organizations, &$id ) {
 			$org = $organization->toArray();
 
-			$org['followers'] = $org['events'] = 0;
+			$org['is_following'] = false;
+			$org['followers']    = $org['events'] = 0;
 
-			OrganizationLocation::where('organization_id', $organization->id)->each(function($location) use (&$org) {
-				$org['followers'] += OrganizationFollower::where('organization_location_id', $location->id)->count();
-				$org['events'] += Event::where('organization_location_id', $location->id)->count();
-			});
+			OrganizationLocation::where( 'organization_id', $organization->id )->each( function ( $location ) use ( &$org, &$id ) {
+				$org['followers'] += OrganizationFollower::where( 'organization_location_id', $location->id )->count();
+				$org['events']    += Event::where( 'organization_location_id', $location->id )->count();
+
+				$org['is_following'] = OrganizationFollower::where( 'organization_location_id', $location->id )
+				                                           ->where( 'account_id', $id )
+				                                           ->exists();
+			} );
 
 			$organizations[] = $org;
-		});
+		} );
 
-		return JSONResponse::encode(Config::get('constants.HTTP_CODES.SUCCESS'), $organizations);
+		return JSONResponse::encode( Config::get( 'constants.HTTP_CODES.SUCCESS' ), $organizations );
 	}
 
 	/**
@@ -211,7 +227,8 @@ class AccountController extends Controller
 			$organization = $follower->organization_location->organization;
 			$org          = $organization->toArray();
 
-			$org['followers'] = $org['events'] = 0;
+			$org['is_following'] = true;
+			$org['followers']    = $org['events'] = 0;
 
 			OrganizationLocation::where( 'organization_id', $organization->id )->each( function ( $location ) use ( &$org ) {
 				$org['followers'] += OrganizationFollower::where( 'organization_location_id', $location->id )->count();
